f2018834459043710a782fea17874115c9e8fdcc
Bmorr1123 <benmorrison1123@gmail.com>
2025-05-09 15:34:54 -0400

    idk

--- util/network_util.lua
+++ util/network_util.lua
@@ -21,23 +21,21 @@ local function find_host(my_job, position, timeout)
     }
 
     for _, computer in pairs(computers) do
-        if computer ~= os.computerID() then
-            print("Pinging "..computer)
-            rednet.send(computer, textutils.serialize(my_data), protocol)
-            
-            local id, response = rednet.receive(protocol, timeout)
-            if id == computer and response == "true" then
-                print("Found Host!")
-                return computer
-            else
-                print("Denied:", response)
-                if response then
-                    response = textutils.unserialize(response)
-                    if type(response) == "table" and response["job"] and response["pos"] then
-                        print("\tHost is not actually a host!")
-                    else 
-                        print(response)
-                    end
+        print("Pinging "..computer)
+        rednet.send(computer, textutils.serialize(my_data), protocol)
+        
+        local id, response = rednet.receive(protocol, timeout)
+        if id == computer and response == "true" then
+            print("Found Host!")
+            return computer
+        else
+            print("Denied:", response)
+            if response then
+                response = textutils.unserialize(response)
+                if type(response) == "table" and response["job"] and response["pos"] then
+                    print("\tHost is not actually a host!")
+                else
+                    print(response)
                 end
             end
         end
